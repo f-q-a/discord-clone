7adfe8d7b58e2d6b210648719ccd1d4e8c89b554
stephenchoung <stephen.choung1@gmail.com>
2021-06-18 21:35:41 -0400

    adjusted models NEED HELP WITH relationship INDEX

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,7 @@
 from .db import db
 from .user import User
+from .servers import Server
+from .channels import Channel
+from .messages import Message
+from .userservers import ServerUser
+from .relationships import Relationship
---
+++ app/models/servers.py
@@ -0,0 +1,26 @@
+from .db import db
+from datetime import datetime
+
+
+class Server(db.Model):
+    __tablename__ = 'servers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    type = db.Column(db.String(8), nullable=True) # Private, Public
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id,
+            'type': self.type,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
+    channels = db.relationship('Channel', back_populates="server")
+    users = db.relationship('User', secondary='server_users', back_populates='servers')
