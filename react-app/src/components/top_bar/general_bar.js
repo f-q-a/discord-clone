4f59e959582dfb7b9a38b548ba548434429c786e
stephenchoung <stephen.choung1@gmail.com>
2021-06-18 20:46:43 -0400

    added basic components files

---
+++ react-app/src/components/top_bar/general_bar.js
@@ -0,0 +1,10 @@
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Redirect } from "react-router-dom";
+
+
+const GeneralBar = () => {
+
+}
+
+export default GeneralBar;
---
+++ react-app/src/components/top_bar/user_bar.js
@@ -0,0 +1,10 @@
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Redirect } from "react-router-dom";
+
+
+const UserBar = () => {
+
+}
+
+export default UserBar;
