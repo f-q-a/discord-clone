2b4bc235bae50750bf5eb888ef1e9a37fbc3b744
stephenchoung <stephen.choung1@gmail.com>
2021-06-21 13:33:44 -0400

    relationship route

--- app/api/relationship_routes.py
+++ app/api/relationship_routes.py
@@ -3,3 +3,11 @@ from app.models import db, Server, User, Channel, Message, ServerUser, Relations
 from flask_login import current_user, login_required
 
 relationship_routes = Blueprint('relationships', __name__)
+
+
+@relationship_routes.route('/')
+def get_relationships ():
+    userId= int(current_user.id)
+    UserRelationship = Relationship.query.filter(Relationship.first_user_id==userId).all()
+    relationships = [relationship.to_dict() for relationship in UserRelationship]
+    return {'relationships': relationships}
