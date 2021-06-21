f1422ede906405fa0102337fe26def0320d5db32
felipe <december1294@gmail.com>
2021-06-21 17:41:37 -0400

    start making message component

--- react-app/src/components/message_main/message_main.js
+++ react-app/src/components/message_main/message_main.js
@@ -1,10 +1,24 @@
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import ServerSidebarItem from "./sidebar_item";
+import React from "react";
+
 // import { Redirect } from "react-router-dom";
 
 
 const MessageMain = () => {
-
+  let { channelId } = useParams();
+  const allMessages = useSelector((state)=> state.messages)
+  print(allMessages)
+  const serversList = servers ? Object.values(servers) : []
+  if (serversList) console.log('<<<<', serversList)
+  // const serversList = []
+  return (
+    <div className='sidebar__div'>
+      <h1>SIDEBAR CONTAINER</h1>
+      {serversList.map((server, index) => <ServerSidebarItem server={server} key={index} />)}
+    </div>
+  )
 }
 
 export default MessageMain;
