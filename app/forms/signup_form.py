8c1b64eb13f4c53c3ce488afad3bbe401fcaf182
felipe <december1294@gmail.com>
2021-06-21 20:08:47 -0400

    fixed signup nightmare

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -8,6 +8,7 @@ def user_exists(form, field):
     print("Checking if user exits", field.data)
     email = field.data
     user = User.query.filter(User.email == email).first()
+    print('HELLO CHECK THIS OUT----->', user)
     if user:
         raise ValidationError("User is already registered.")
 
