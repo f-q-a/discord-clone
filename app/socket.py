48408cff90a183909915fcc6ffa96fb73134e56f
felipe <december1294@gmail.com>
2021-06-22 13:11:12 -0400

    add socketio to backend and frontend

--- app/socket.py
+++ app/socket.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
 import os
 
 # create your SocketIO instance
@@ -18,4 +18,21 @@ socketio = SocketIO(cors_allowed_origins=origins)
 
 @socketio.on("chat")
 def handle_chat(data):
+
+    """
+    msgArr = []
+    message = Message(user_id=sender_user_id,
+                      content = data.msg
+                      channel_id = data.channelId)
+    msgArr.append(message)
+    """
+    
+    print('SOCKETIO DATA HERE ------>', data)
     emit("chat", data, broadcast=True)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
---
+++ react-app/src/components/chat/chat.js
@@ -0,0 +1,61 @@
+
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { io } from 'socket.io-client'
+import * as messageActions from '../../store/message'
+
+let socket
+
+const Chat = () => {
+    const user = useSelector(state => state.session.user)
+
+    const [messages, setMessages] = useState([])
+    const [chatInput, setChatInput] = useState("")
+
+    useEffect(() => {
+        socket = io()
+
+        socket.on('chat', (chat) => {
+            setMessages(messages => [...messages, chat])
+            console.log()
+            messageActions.createMessage(chat)
+        })
+
+        return (() => {
+            socket.disconnect()
+        })
+    }, [])
+
+    const updateChatInput = (e) => {
+        e.preventDefault()
+        setChatInput(e.target.value)
+    }
+
+    const sendChat = (e) => {
+        e.preventDefault()
+        socket.emit('chat', { user: user.username, msg: chatInput })
+        console.log('<<<<<<<', messages)
+        setChatInput("")
+    }
+
+
+    return (user && (
+        <div>
+            <div>
+                {messages.map((message, idx) => (<div key={idx}>
+                    {`${message.user}: ${message.msg}`}</div>
+                ))}
+            </div>
+            <form onSubmit={sendChat}>
+                <input
+                    value={chatInput}
+                    onChange={updateChatInput}
+                />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    )
+    )
+}
+
+export default Chat
