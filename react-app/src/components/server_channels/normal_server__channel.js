38882c8730a7a5499c32177155677028db4ccea6
Bill Adams <vikingbill@gmail.com>
2021-06-24 19:28:34 -0400

    some progress

--- react-app/src/components/server_channels/normal_server__channel.js
+++ react-app/src/components/server_channels/normal_server__channel.js
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
 import * as messageActions from '../../store/message'
 
 function NormalChannel({ channel }) {
-  const  {serverId} = useParams();
+  const  {serverId, channelId} = useParams();
   const dispatch = useDispatch();
+  const [activeDiv, setActiveDiv] = useState('')
   useEffect(()=>{
     dispatch(messageActions.getMessages(channel.id))
   },[serverId])
   return (
-    <div className="normal_channel__div">
+    <div className={`normal_channel__div ${activeDiv}`}>
       <p>Channel</p>
       <NavLink
         className="channel_list__link"
         exact
-        to={`/@me/${channel.server_id}/${channel.id}`}>
-          {channel.name}
+        to={`/@me/${channel.server_id}/${channel.id}`}
+        onClick={()=> {
+          setActiveDiv('active__div')
+          }}>
+          # {channel.name}
         </NavLink>
     </div>
   );
