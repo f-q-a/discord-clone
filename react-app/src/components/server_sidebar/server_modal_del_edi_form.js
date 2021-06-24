083143d3ee4ce8087c8f3da474a959e995af5bcc
stephenchoung <stephen.choung1@gmail.com>
2021-06-24 17:13:39 -0400

    mid day

--- react-app/src/components/server_sidebar/server_modal_del_edi_form.js
+++ react-app/src/components/server_sidebar/server_modal_del_edi_form.js
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal } from '../../context/Modal';
 
 import ServerDeleteForm from './server_delete_form'
 import ServerEditForm from './server_edit_form'
+import ServerAddUserForm from './server_adduser_form'
 
-function ServerFormModal({item, serverId}) {
+function ServerFormModal({item,serverId}) {
   const [showModal, setShowModal] = useState(false);//false
+  const [display, setDisplay] = useState(true);
+
+  useEffect(() => {
+    if (item.text === "Edit Server Name" || item.text === "Delete Server"){
+      setDisplay(true)
+    }
+    else if (item.text==="Add Friend") {
+      setDisplay(false)
+    }
+  },[])
+
 
   return (
     <>
         <a className="LoginButton" onClick={() => setShowModal(true)}>{item.text}</a>
         {showModal && (
         <Modal onClose={() => setShowModal(false)}>
+            {display ?
             <div>
               <ServerDeleteForm serverId={serverId}/>
               <ServerEditForm serverId={serverId}/>
             </div>
+            :
             <div>
-              
+               <ServerAddUserForm serverId ={serverId}/>
             </div>
+            }
         </Modal>
         )}
     </>
