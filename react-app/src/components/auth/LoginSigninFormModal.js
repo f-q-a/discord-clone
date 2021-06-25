e4c6af6eff8c10986cdd38768ceca9bd7eabfeee
stephenchoung <stephen.choung1@gmail.com>
2021-06-25 01:50:45 -0400

    grid

--- react-app/src/components/auth/LoginSigninFormModal.js
+++ react-app/src/components/auth/LoginSigninFormModal.js
@@ -4,6 +4,7 @@ import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm'
 // import './LoginFormModal.css';
+import "../css/navigation.css"
 
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
@@ -18,7 +19,7 @@ function LoginFormModal() {
   };
   return (
     <>
-      <button className="LoginButton" onClick={() => setShowModal(true)}>Log In</button>
+      <button className="LoginButton button" onClick={() => setShowModal(true)}>Log In</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           {formState ?
