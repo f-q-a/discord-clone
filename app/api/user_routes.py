bcb594a0ecc9aa8b5d4291b57db7f4deae1358b6
stephenchoung <stephen.choung1@gmail.com>
2021-06-24 21:37:02 -0400

    eod

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask import Blueprint, jsonify, request
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import db, Server, User, Channel, Message, ServerUser, Relationship
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,31 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:id>', methods=['PUT'])
+def edit_user(id):
+    user_id = int(current_user.id)
+    res = request.get_json()
+    user = db.session.query(User).get(user_id)
+    if(res['password'] != res['repeatPassword']):
+        return {errors:["Password Does Not Match"]}
+    else:
+        if(user.username != res['username']):
+            user.username = res['username']
+        elif(user.email != res['email']):
+            user.email = res['email']
+        elif(user.avatar_link != res['image']):
+            user.avatar_link = res['image']
+        elif(user.hashed_password!= res['password']):
+            user.hashed_password = res['password']
+        db.session.add(user)
+        db.session.commit()
+        return user.to_dict()
+
+@user_routes.route('/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user_id = int(current_user.id)
+    user= db.session.query(User).get( user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return {}
