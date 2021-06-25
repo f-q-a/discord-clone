da3f2d6fd17beae29e73eaf5f45cf87f5bba45e9
Bill Adams <vikingbill@gmail.com>
2021-06-25 19:39:10 -0400

    resolve merge conflicts

--- react-app/src/components/app_container/main_app.js
+++ react-app/src/components/app_container/main_app.js
@@ -6,13 +6,9 @@ import * as serverActions from "../../store/server";
 
 function MainApp() {
 
-<<<<<<< HEAD
-=======
-
->>>>>>> main
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(serverActions.getServers()).then((data) => console.log(data));
+    dispatch(serverActions.getServers())
 
 
 
@@ -20,19 +16,12 @@ function MainApp() {
 
 
   return (
-<<<<<<< HEAD
     <>
       <div className="sidebar__container">
         <ServerSidebar />
 
       </div>
     </>
-=======
-    <div className="sidebar__container">
-      <ServerSidebar />
-        
-    </div>
->>>>>>> main
   );
 }
 
