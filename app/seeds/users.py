4866bbebfd2ea3aca004f45c4fb5b09c17dcc439
Bill Adams <vikingbill@gmail.com>
2022-03-18 23:44:09 -0400

    more aws, bucket is public

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,11 +1,25 @@
 from werkzeug.security import generate_password_hash
 from app.models import db, User
 from faker import Faker
+import random
 
 # Adds a demo user, you can add other users here if you want
 faker = Faker()
 # Adds a demo user, you can add other users here if you want
 
+avatars = [
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-100.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-24.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-24.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-32.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-48.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-50.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-64.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-new-24.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-new-48.png',
+    'https://discord-clone-bill.s3.us-east-2.amazonaws.com/icons8-discord-new-64.png'
+]
+    
 
 def seed_users():
     hash_password = generate_password_hash('password')
@@ -16,7 +30,7 @@ def seed_users():
     for i in range(2, 100):
         hash_password = generate_password_hash('password')
         temp = User(username=faker.user_name(), email=faker.email(),
-                    hashed_password=hash_password, avatar_link="https://discord.com/assets/6f26ddd1bf59740c536d2274bb834a05.png")
+                    hashed_password=hash_password, avatar_link=random.choice(avatars))
         db.session.add(temp)
         db.session.commit()
 
